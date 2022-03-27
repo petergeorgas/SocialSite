be70430f0ef9c57dcbfd88303b9ad003e006017b
Peter Georgas <peter_georgas@yahoo.com>
2022-03-27 15:29:39 -0400

    Add log4j, changed default port

--- src/main/java/com/goons/socialsite/SocialsiteApplication.java
+++ src/main/java/com/goons/socialsite/SocialsiteApplication.java
@@ -1,6 +1,7 @@
 package com.goons.socialsite;
 
 import com.goons.socialsite.config.SpringJdbcConfig;
+import com.goons.socialsite.util.Logging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,8 +21,10 @@ public class SocialsiteApplication {
 
         try {
             dataSource.getConnection(); // Confirm that we have a connection to the DB.
+            Logging.logger.info("Successfully connected to Postgres");
         } catch (SQLException e){
-            e.printStackTrace();
+            Logging.logger.error(e);
+            System.exit(-1);
         }
 
 
---
+++ src/main/java/com/goons/socialsite/util/Logging.java
@@ -0,0 +1,9 @@
+package com.goons.socialsite.util;
+
+import com.goons.socialsite.SocialsiteApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Logging {
+    public static final Logger logger = LogManager.getLogger(SocialsiteApplication.class);
+}
